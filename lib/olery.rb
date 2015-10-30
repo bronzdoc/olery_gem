3f52d01e6b7ec5cab7a95c9936f34a821c704d6b
bronzdoc <lsagastume1990@gmail.com>
2015-10-29 19:20:56 -0600

    Just include the libs

--- lib/olery.rb
+++ lib/olery.rb
@@ -1,5 +1,5 @@
 require "olery/version"
+require_relative "olery/configuration"
+require_relative "olery/resource"
+require_relative "olery/inspector"
 
-module Olery
-  # Your code goes here...
-end
