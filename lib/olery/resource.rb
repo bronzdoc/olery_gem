7ce9e126be90319817523c8835ddf7125be5db13
bronzdoc <lsagastume1990@gmail.com>
2015-10-29 19:19:25 -0600

    Cleaning resources

--- lib/olery/resource.rb
+++ lib/olery/resource.rb
@@ -2,28 +2,33 @@ require_relative "robot"
 
 module Olery
 
-
   # <Resource> represents an endpoint we should hit to get the desired result page.
   class Resource
-    attr_accessor :host, :action, :method, :search_key
-    attr_reader :page
+    attr_accessor :host, :action, :method, :search_key, :extra_params
 
-    def initialize(host, action, method, search_key, extra_params={})
+    def initialize(host, action, method, search_key, extra_params={}, parser)
       @host = host
       @action = action
       @method = method.downcase
       @search_key = search_key
       @extra_params = extra_params
       @bot = Olery::Robot.new.agent
+      @parser = parser
     end
 
+    def scrape(word)
+      page = search word
+      @parser.call(page, word)
+    end
+
+    private
     # Hit the search endpoint with the specified params.
     # The params argument should include the word we are searching.
     # e.g params = {name: "DoubleTree Hilton Amsterdam"}, assuming "name"
     # is the search argument the query endpoint is expecting.
-    # @returns <Mechanize::Page>
     def search(word)
-      @bot.send(@method, "http://#{@host}#{@action}", {@search_key => word}.merge(@extra_params))
+      html_page = @bot.send(@method, "http://#{@host}#{@action}", {@search_key => word}.merge(@extra_params)).body
+      Nokogiri::HTML(html_page)
     end
   end
 end
