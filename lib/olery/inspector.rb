4c88c6bd2c7340753b4a540b0f6033fd70a75061
bronzdoc <lsagastume1990@gmail.com>
2015-10-29 19:20:01 -0600

    Removded funcionality

--- lib/olery/inspector.rb
+++ lib/olery/inspector.rb
@@ -9,35 +9,17 @@ module Olery
       @results_queue = Queue.new
     end
 
-    # Inspect the the configuratin resoruces
+    # Inspect configuration resoruces
     def inspect
       Olery.configuration.resources.each do |resource|
         puts "Looking for hotels in #{resource.host} that match the word \"#{@word}\""
-        result = analyze resource
-        puts "#{result[:links_found]} links found in #{resource.host}"
-
-        if result[:links_found] > 0
-          link = result[:matching_link]
-          puts "Queuing link #{link}"
-          @results_queue << link.href
-        end
-        puts "\n"
+        run resource
       end
-
-      @results_queue
     end
 
     private
-    def analyze(resource)
-      puts "Analyzing resource..."
-      page = resource.search(@word)
-      #page.links.each do |l|
-      #  p "#{l.text} => #{l.href}"
-      #end
-      links = page.links.select {|l| l.text =~ /#{@word}/i }
-      return {links_found: 0, matching_link: nil} if links.nil?
-
-      {links_found: links.size, matching_link: links.first}
+    def run(resource)
+      resource.scrape @word
     end
   end
 end
